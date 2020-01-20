201bd6880d2e5b6ced603f6688e2e0a9f06213a8
abdirahmanfarah <abdirahman.z.farah@gmail.com>
2020-01-19 17:26:44 -0800

    MVP;

--- queries.sql
+++ queries.sql
@@ -2,15 +2,27 @@
 
 -- Find all customers with postal code 1010
 
+SELECT * FROM customers WHERE postalCode=1010;
+
 -- Find the phone number for the supplier with the id 11
+
+SELECT phone FROM suppliers WHERE supplierID = 11;
 
 -- List first 10 orders placed, sorted descending by the order date
 
+SELECT * FROM orders ORDER BY OrderDate DESC LIMIT 10;
+
 -- Find all customers that live in London, Madrid, or Brazil
+
+SELECT * FROM customers WHERE city="London" OR city="Madrid" OR country="Brazil"
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 
+INSERT INTO Customers(CustomerName, ContactName, Address, City, PostalCode, Country) VALUES("The Shire", "Bilbo Baggins", "1 Hobbit-Hole", "Bag End", "111", "Middle Earth");
+
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+
+UPDATE customers SET postalCode = 11122 WHERE customerId = 92;
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
+// const db = require('./data/dbConfig.js');
+
+const accountsRouter = require('./accounts/accountsRouter.js');
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', accountsRouter);
+
+server.use((err, req, res, next) => {
+  console.log(err)
+  res.status(500).json({message: "Something went very wrong!"})
+})
+
+server.get('/', (req, res) => {
+  res.send(`<h2>Ready to get Started</h2>`)
+})
+
 
 module.exports = server;
